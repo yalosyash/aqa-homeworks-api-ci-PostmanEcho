df38e2c09df1a173b10bc4595857a14a4e31470e
Vladislav Klepikov <ya.losyash@yandex.ru>
2024-04-11 19:38:53 +0300

    init commit

---
+++ src/test/java/PostmanEchoTestV1.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV3 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+//                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
